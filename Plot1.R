77502edd6660f07d43523c64d8ee3ba8338e9d26
Lakna Jayasinghe <lakna1315.jayasinghe@gmail.com>
2019-08-09 20:30:42 -0700

    Changes to plot1.R and plot2.R

--- Plot1.R
+++ Plot1.R
@@ -1,7 +1,6 @@
 file <- "household_power_consumption.txt"
 powerData <- read.table(file, header = TRUE, sep = ";", na.strings = "?")
 powerData <- powerData[powerData$Date %in% c("1/2/2007","2/2/2007") ,]
-globalActivePower <- as.numeric(powerData$Global_active_power)
 png("plot1.png", width=480, height=480)
-hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+hist(powerData$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
--- plot2.R
+++ plot2.R
@@ -2,8 +2,7 @@ file <- "household_power_consumption.txt"
 powerData <- read.table(file, header = TRUE, sep = ";", na.strings = "?", stringsAsFactors=FALSE, dec=".")
 powerData <- powerData[powerData$Date %in% c("1/2/2007","2/2/2007") ,]
 dateTime <- strptime(paste(powerData$Date, powerData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
-globalActivePower <- as.numeric(powerData$Global_active_power)
 png("plot2.png", width=480, height=480)
-plot(dateTime, globalActivePower,type="l",xlab="",ylab="Global Active Power (kilowatts)")
+plot(dateTime, powerData$Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)")
 dev.off()
 
