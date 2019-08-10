8283b2f14443a273c3995208359c3507d1c96224
Lakna Jayasinghe <lakna1315.jayasinghe@gmail.com>
2019-08-09 20:24:44 -0700

    Course 4 Project 1

---
+++ plot3.R
@@ -0,0 +1,11 @@
+file <- "household_power_consumption.txt"
+powerData <- read.table(file, header = TRUE, sep = ";", na.strings = "?", stringsAsFactors=FALSE, dec=".")
+powerData <- powerData[powerData$Date %in% c("1/2/2007","2/2/2007") ,]
+dateTime <- strptime(paste(powerData$Date, powerData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+png("plot3.png", width=480, height=480)
+plot(dateTime, powerData$Sub_metering_1,type="l",xlab="",ylab="Energy sub metering", col = "black")
+lines(dateTime, powerData$Sub_metering_2, type="l", col = "red")
+lines(dateTime, powerData$Sub_metering_3, type="l", col = "blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, lwd = 2.5, 
+       col = c("black", "red", "blue"))
+dev.off()
